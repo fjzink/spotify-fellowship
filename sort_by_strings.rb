ca803530651b7619b6e1ff3a74bbf8a78c4000e9
Fulton Zink <joeyzink@gmail.com>
2018-06-11 15:23:29 -0700

    Add solution for sort_by_strings.

--- sort_by_strings.rb
+++ sort_by_strings.rb
@@ -10,18 +10,24 @@ def sort_by_strings(s, t)
     end
 
     s_array = s.split(//)
+
     j = 0
     while j < s_array.length
         s_array[j] = char_to_num[s_array[j]]
         j += 1
     end
+
     s_array = s_array.sort
+
     k = 0
     while k < s_array.length
-
+        s_array[k] = num_to_char[s_array[k]]
         k += 1
     end
-    
+
+    sorted_word = s_array.join
+    return sorted_word
 end
 
-sort_by_strings("dog", "odg")
+p sort_by_strings("weather", "therapyw")
+p sort_by_strings("good", "odg")
